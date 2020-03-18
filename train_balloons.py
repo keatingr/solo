41153f8cd5fbff7346304ed5f26676d09217e6c9
Mark OBrien <mark_noreply@noreply.com>
2020-03-17 22:06:44 -0400

    Fix val folder

--- train_balloons.py
+++ train_balloons.py
@@ -82,11 +82,11 @@ if __name__ == '__main__':
     cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7  # set the testing threshold for this model
     # Set training data-set path
-    cfg.DATASETS.TEST = ("balloon/val",)
+    cfg.DATASETS.TEST = ("./images/balloon/val",)
     # Create predictor (model for inference)
     predictor = DefaultPredictor(cfg)
 
-    dataset_dicts = get_balloon_dicts("balloon/val")
+    dataset_dicts = get_balloon_dicts("./images/balloon/val")
     for d in random.sample(dataset_dicts, 3):
         im = cv2.imread(d["file_name"])
         outputs = predictor(im)
@@ -96,5 +96,5 @@ if __name__ == '__main__':
                        instance_mode=ColorMode.IMAGE_BW  # remove the colors of unsegmented pixels
                        )
         v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-        cv2.imshow(v.get_image()[:, :, ::-1])
+        cv2.imshow('', v.get_image()[:, :, ::-1])
         cv2.waitKey()
