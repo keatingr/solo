fac1ee486e747c27e588c7cfc6355479b30621de
Mark OBrien <mark_noreply@noreply.com>
2020-03-17 23:56:52 -0400

    Cleanup solo train

--- train_solo.py
+++ train_solo.py
@@ -1,44 +1,64 @@
 import os
-import numpy as np
-import json
-from detectron2.structures import BoxMode
-import itertools
-import cv2
-from detectron2.engine import DefaultTrainer
-from detectron2.config import get_cfg
-from detectron2.data import DatasetCatalog, MetadataCatalog
-from detectron2.utils.visualizer import ColorMode
 import random
-from detectron2.engine import DefaultPredictor
+
+import cv2
 from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer
+from detectron2.data import DatasetCatalog
 from detectron2.data import MetadataCatalog
+from detectron2.data.datasets import register_coco_instances
+from detectron2.engine import DefaultPredictor
+from detectron2.engine import DefaultTrainer
+from detectron2.utils.visualizer import ColorMode
+from detectron2.utils.visualizer import Visualizer
+
+
+def verify_dataload():
+    """
+    To ensure proper loading display a random member from the dataset with annotation
+    :return:
+    """
+    import random
+
+    for d in random.sample(dataset_dicts, 3):
+        img = cv2.imread(d["file_name"])
+        visualizer = Visualizer(img[:, :, ::-1], metadata=solo_metadata, scale=0.5)
+        vis = visualizer.draw_dataset_dict(d)
+        cv2.imshow('', vis.get_image()[:, :, ::-1])
+        cv2.waitKey()
+
+
+def train():
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+    trainer = DefaultTrainer(cfg)
+    trainer.resume_or_load(resume=False)
+    trainer.train()
+
+
+def predict():
+    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7  # set the testing threshold for this model
+    # Set training data-set path
+    cfg.DATASETS.TEST = ("./images/balloon/val",)
+    # Create predictor (model for inference)
+    predictor = DefaultPredictor(cfg)
+
+    for d in random.sample(dataset_dicts, 3):
+        im = cv2.imread(d["file_name"])
+        outputs = predictor(im)
+        v = Visualizer(im[:, :, ::-1],
+                       metadata=solo_metadata,
+                       scale=0.8,
+                       instance_mode=ColorMode.IMAGE_BW  # remove the colors of unsegmented pixels
+                       )
+        v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+        cv2.imshow('', v.get_image()[:, :, ::-1])
+        cv2.waitKey()
 
 
 if __name__ == '__main__':
-
-    # for d in ["train", "val"]:
-    #     DatasetCatalog.register("./images/balloon/" + d, lambda d=d: get_balloon_dicts("./images/balloon/" + d))
-    #     MetadataCatalog.get("./images/balloon/" + d).set(thing_classes=["balloon"])
-    # balloon_metadata = MetadataCatalog.get("./images/balloon/train")
-
-    from detectron2.data.datasets import register_coco_instances
-
     register_coco_instances("solo_dataset", {}, "./solo.json", "./traindata/")
-    from detectron2.data import MetadataCatalog
-
     solo_metadata = MetadataCatalog.get("solo_dataset")  #.thing_classes = ["solo"]
     dataset_dicts = DatasetCatalog.get("solo_dataset")
-
-    # To ensure proper loading display a random member from the dataset with annotation
-    # import random
-    #
-    # for d in random.sample(dataset_dicts, 3):
-    #     img = cv2.imread(d["file_name"])
-    #     visualizer = Visualizer(img[:, :, ::-1], metadata=solo_metadata, scale=0.5)
-    #     vis = visualizer.draw_dataset_dict(d)
-    #     cv2.imshow('', vis.get_image()[:, :, ::-1])
-    #     cv2.waitKey()
 
     cfg = get_cfg()
     cfg.merge_from_file("./configs/mask_rcnn_R_50_FPN_3x.yaml")  #./detectron2_repo/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml
@@ -52,28 +72,5 @@ if __name__ == '__main__':
     cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128  # faster, and good enough for this toy dataset
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon)
 
-    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-    trainer = DefaultTrainer(cfg)
-    trainer.resume_or_load(resume=False)
-    trainer.train()
-
-    # # load weights
-    # cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
-    # cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7  # set the testing threshold for this model
-    # # Set training data-set path
-    # cfg.DATASETS.TEST = ("./images/balloon/val",)
-    # # Create predictor (model for inference)
-    # predictor = DefaultPredictor(cfg)
-    #
-    # dataset_dicts = get_balloon_dicts("./images/balloon/val")
-    # for d in random.sample(dataset_dicts, 3):
-    #     im = cv2.imread(d["file_name"])
-    #     outputs = predictor(im)
-    #     v = Visualizer(im[:, :, ::-1],
-    #                    metadata=balloon_metadata,
-    #                    scale=0.8,
-    #                    instance_mode=ColorMode.IMAGE_BW  # remove the colors of unsegmented pixels
-    #                    )
-    #     v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-    #     cv2.imshow('', v.get_image()[:, :, ::-1])
-    #     cv2.waitKey()
+    train()
+    predict()
